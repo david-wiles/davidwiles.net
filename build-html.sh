3882d7a4394aa6870d744c493a1220523273e04a
David Wiles <david@wiles.fyi>
2024-01-21 18:37:21 -0600

    Use haskell template engine

--- build-html.sh
+++ build-html.sh
@@ -4,15 +4,10 @@
 for p in $(find html -name '*.html' -not -path 'html/templates/*'); do
   OUTFILE="www/${p#html/}";
   VARFILE="$(dirname $p)/$(basename $p .html).vars"
+  DATAFILE="$(dirname $p)/.vars"
 
   mkdir -p $(dirname "${OUTFILE}")
 
   # If the filename is list.html, then set the file's directory as -vd
-  if [ $(basename $p) == "list.html" ]; then
-    tmpl -of ${OUTFILE} -vd $(dirname $p) $p;
-  elif [ -f "${VARFILE}" ]; then
-    tmpl -of ${OUTFILE} -vf ${VARFILE} $p;
-  else
-    tmpl -of ${OUTFILE} $p;
-  fi
+  tmpl -v ${DATAFILE} -v ${VARFILE} -o ${OUTFILE} $p
 done
